548e3a79567c93fb2c60f79dcf1d3bd87bd035f6
Nikita Bikmaev <nikitabikmaev@gmail.com>
2019-05-15 13:33:55 +0300

    Init

---
+++ translit.go
@@ -0,0 +1,62 @@
+package translit
+
+import (
+	"strings"
+	"unicode"
+)
+
+var cyrillic = map[int32]string{
+	'а': "a",
+	'б': "b",
+	'в': "v",
+	'г': "g",
+	'д': "d",
+	'е': "e",
+	'ё': "yo",
+	'ж': "zh",
+	'з': "z",
+	'и': "i",
+	'й': "j",
+	'к': "k",
+	'л': "l",
+	'м': "m",
+	'н': "n",
+	'о': "o",
+	'п': "p",
+	'р': "r",
+	'с': "s",
+	'т': "t",
+	'у': "u",
+	'ф': "f",
+	'х': "h",
+	'ц': "c",
+	'ч': "ch",
+	'ш': "sh",
+	'щ': "sch",
+	'ъ': "",
+	'ы': "y",
+	'ь': "",
+	'э': "e",
+	'ю': "ju",
+	'я': "ja",
+	' ': "_",
+}
+
+// ToCyrillic translits cyrillic to latin
+func ToCyrillic(text string) string {
+	out := make([]string, len(text))
+	for _, t := range text {
+		if c, ok := cyrillic[t]; ok {
+			out = append(out, c)
+		} else {
+			if unicode.IsUpper(t) {
+				if c, ok := cyrillic[unicode.ToLower(t)]; ok {
+					out = append(out, string(strings.ToUpper(c)))
+					continue
+				}
+			}
+			out = append(out, string(t))
+		}
+	}
+	return strings.Join(out, "")
+}
---
+++ translit_test.go
@@ -0,0 +1,20 @@
+package translit
+
+import (
+	"testing"
+)
+
+func BenchmarkTest(b *testing.B) {
+	s := "АаБбВвГгДдЕе ЁёЖжЗзИиЙйКкЛ лМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщ"
+	for i := 0; i < b.N; i++ {
+		ToCyrillic(s)
+	}
+}
+
+func TestTranslit(t *testing.T) {
+	s := "АаБбВвГгДдЕе ЁёЖжЗзИиЙйКкЛ лМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
+	out := ToCyrillic(s)
+	if out != "AaBbVvGgDdEe_YOyoZHzhZzIiJjKkL_lMmNnOoPpRrSsTtUuFfHhCcCHchSHshSCHschYyEeJUjuJAja" {
+		t.Errorf("%s != AaBbVvGgDdEe_YOyoZHzhZzIiJjKkL_lMmNnOoPpRrSsTtUuFfHhCcCHchSHshSCHschYyEeJUjuJAja", out)
+	}
+}
